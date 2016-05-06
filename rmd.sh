ea3f992e9b3569bc634dddedf7e4d71820d309b2
Michael <micke.green@gmail.com>
2016-05-06 10:36:05 +0200

    Added useful scripts for parsing Rmd files and converting to markdown.

---
+++ rmd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#echo "${BASH_SOURCE[0]}"
+#echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+Rscript -e "source('$DIR/rmarkdown.R'); convertRMarkdown(images.dir='$DIR/images')"
