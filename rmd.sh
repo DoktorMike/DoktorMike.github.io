d8c20cefaad3096500207d8d327b7b0e5532d613
Michael <micke.green@gmail.com>
2016-07-24 15:46:10 +0200

    Added a new post.

--- rmd.sh
+++ rmd.sh
@@ -3,3 +3,4 @@
 #echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 Rscript -e "source('$DIR/rmarkdown.R'); convertRMarkdown(images.dir='$DIR/images')"
+mv *.md ../_posts
